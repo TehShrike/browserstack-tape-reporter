95ffefa050b112c34f64ba9dbed57de4ebbbe073
Josh Duff <me@JoshDuff.com>
2016-08-19 11:43:03 -0500

    Whitespace refactor

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ var start = new Date()
 var logs = ''
 
 tape.createStream()
-.on('data', storeLogs)
-.on('close', pollReport)
+	.on('data', storeLogs)
+	.on('close', pollReport)
 
 function storeLogs (data) {
-  logs += data
+	logs += data
 }
 
 function pollReport () {
-  window.BrowserStack
-  ? postLogs()
-  : setTimeout(pollReport, 500)
+	window.BrowserStack
+  	? postLogs()
+  	: setTimeout(pollReport, 500)
 }
 
 function postLogs () {
-  logs
-  ? window.BrowserStack.post('/_log', '\n' + logs + '\n ', postReport)
-  : postReport()
+	logs
+  	? window.BrowserStack.post('/_log', '\n' + logs + '\n ', postReport)
+  	: postReport()
 }
 
 function postReport () {
-  var results = tape.getHarness()._results
-  window.BrowserStack.post('/_report', {
-    runtime: new Date().getTime() - start.getTime(),
-    total: results.count,
-    passed: results.pass,
-    failed: results.fail,
-    tracebacks: []
-  }, function () {})
+	var results = tape.getHarness()._results
+	window.BrowserStack.post('/_report', {
+		runtime: new Date().getTime() - start.getTime(),
+		total: results.count,
+		passed: results.pass,
+		failed: results.fail,
+		tracebacks: []
+	}, function () {})
 }
